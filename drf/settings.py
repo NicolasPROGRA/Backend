b8266d2816033ef6a0967523afa104aef6d3dc8b
NicolasPROGRA <nicolasc101004@gmail.com>
2025-08-21 16:05:00 -0400

    ola

--- drf/settings.py
+++ drf/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'drf.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'iei_171_n4',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
