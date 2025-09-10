33b7e3ef897962a59b28a6cc3bb15933468c30a5
NICOPROGRA <nicolasc101004@gmail.com>
2025-09-10 17:43:19 -0300

    cambios

--- primera_app/admin.py
+++ primera_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Nacionalidad, Autor, Comuna, Direccion, Biblioteca, Lector, Libro, Prestamo
+from .models import Nacionalidad, Autor, Comuna, Direccion, Biblioteca, Lector, TipoCategoria, Categoria, Libro, Prestamo
+
 # Register your models here.
 admin.site.register(Nacionalidad)
 admin.site.register(Autor)
@@ -7,5 +8,7 @@ admin.site.register(Comuna)
 admin.site.register(Direccion)
 admin.site.register(Biblioteca)
 admin.site.register(Lector)
+admin.site.register(TipoCategoria)
+admin.site.register(Categoria)
 admin.site.register(Libro)
 admin.site.register(Prestamo)
