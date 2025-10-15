3edbb17ac237b2967a31cb97ea1ddba7b415c01a
NICOPROGRA <nicolasc101004@gmail.com>
2025-10-15 18:08:07 -0300

    cambios

--- drf/urls.py
+++ drf/urls.py
@@ -17,9 +17,32 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from primera_app import views
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Documentación API Gestión de Biblioteca",
+        default_version='v1',
+        description="Aplicación de Gestión de Biblioteca",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="mi_correo@test.test"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.pagina_html, name='home'),
-    path('primera_app/', include ('primera_app.urls'))
+    path('primera_app/', include ('primera_app.urls')),
+    
+    # URL's para documentación de API
+    path('apidocs/', schema_view.with_ui('swagger',cache_timeout=0), name='schema-swagger-ui'),                                    
+    path('redocs/', schema_view.with_ui('redoc',cache_timeout=0), name='schema-redoc'),
+                                        
+
 ]
+
+
