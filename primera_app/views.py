1b6db57fd8756ae88f59aa3693dd907950eda4bc
NICOPROGRA <nicolasc101004@gmail.com>
2025-10-22 17:53:46 -0300

    cambios 22-10

--- primera_app/views.py
+++ primera_app/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+
+from django.contrib.auth import logout, login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
 from .serializer import Nacionalidad_Serializer, Autor_Serializer, Comuna_Serializer, Direccion_Serializer, Biblioteca_Serializer, Libro_Serializer, TipoCategoria_Serializer, Categoria_Serializer, Lector_Serializer, Prestamo_Serializer
 from .models import Nacionalidad, Autor, Comuna, Direccion, Biblioteca, Lector, TipoCategoria, Categoria, Libro, Prestamo
 
@@ -60,3 +67,27 @@ class PrestamoViewSet(viewsets.ModelViewSet):
 def pagina_html(request):
     return render(request, 'pagina.html')
 
+def logout_view(request):
+    # Cierra la sesión del usuario y limpia la data de SESSION
+    logout(request)
+    # Redirige a la página de inicio de sesión
+    return redirect('login')
+
+def registro(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Registro Exitoso. ¡Bienvenido!")
+            return redirect('/')
+        else:
+            messages.error(
+                request, "No ha sido posible Registrarlo. Por favor revise el formulario por errores.")
+    else:
+        form = UserCreationForm()
+    return render(request, 'registro.html', {'form': form})
+
+@login_required
+def pagina_inicio(request):
+    return render(request, 'primera_app/pagina.html')
